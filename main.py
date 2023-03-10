da9fb39fcc3a07f4ce9168bd9009ac147b7eb382
uniqueFranky <y_r_b@qq.com>
2023-03-10 20:42:38 +0800

    add mask

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ tokenizer = torchtext.data.utils.get_tokenizer('spacy', language='en_core_web_sm
 file_path = 'human_chat.txt'
 max_sentence_length = 50
 
+
 def build_vocab() -> (torchtext.vocab.vocab, int):
     counter = collections.Counter()
     num_sentences = 0
@@ -23,9 +24,25 @@ def build_vocab() -> (torchtext.vocab.vocab, int):
 corpus_vocab, corpus_num_sentences = build_vocab()
 corpus_itos = corpus_vocab.get_itos()
 corpus_stoi = corpus_vocab.get_stoi()
+corpus_vocab_size = len(corpus_stoi)
 
 
-def random_choose_and_pad(is_next=False) -> [int]:
+def mask(tokens: [int]) -> ([int], [int]):
+    num_mask = int(0.15 * len(tokens))
+    masked_positions = [i for i in range(0, len(tokens)) if tokens[i] > 3]
+    random.shuffle(masked_positions)
+    masked_positions = masked_positions[: num_mask]
+    masked_tokens = [tokens[pos] for pos in masked_positions]
+    for pos in masked_positions:
+        if random.random() < 0.8:
+            tokens[pos] = corpus_stoi['[MASK]']
+        elif random.random() < 0.5:
+            tokens[pos] = random.randint(corpus_stoi['[MASK]'] + 1, corpus_vocab_size)
+    return masked_positions, masked_tokens
+
+
+def random_choose_and_pad_and_mask(is_next=False) -> ([int], [int], [int]):
+    # random choose
     pos1 = random.randint(0, corpus_num_sentences - 1)
     pos2 = pos1 + 1 if is_next else random.randint(pos1 + 2, corpus_num_sentences)
     tokens1 = []
@@ -39,24 +56,26 @@ def random_choose_and_pad(is_next=False) -> [int]:
                 for token in tokenizer(line.rstrip('\n')):
                     tokens2.append(corpus_stoi[token])
                 break
+
+    # pad
     while len(tokens1) < max_sentence_length:
         tokens1.append(corpus_stoi['[PAD]'])
     while len(tokens2) < max_sentence_length:
         tokens2.append(corpus_stoi['[PAD]'])
-    return [corpus_stoi['[CLS]']] + tokens1 + [corpus_stoi['[SEP]']] + tokens2
+    tokens = [corpus_stoi['[CLS]']] + tokens1 + [corpus_stoi['[SEP]']] + tokens2
+    masked_positions, masked_tokens = mask(tokens)
+    return tokens, masked_positions, masked_tokens
 
 
-# returns a list in shape
 def generate_batch() -> list:
     batch = []
     for _ in range(int(batch_size / 2)):
-        tokens = random_choose_and_pad()
-        masked_tokens, masked_positions = mask_tokens(tokens)
+        tokens, masked_positions, masked_tokens = random_choose_and_pad_and_mask(is_next=False)
+        batch.append((tokens, masked_positions, masked_tokens, False))
+
+        tokens, masked_positions, masked_tokens = random_choose_and_pad_and_mask(is_next=True)
+        batch.append((tokens, masked_positions, masked_tokens, True))
     return batch
-
-
-def mask_tokens(tokens: [int]) -> ([int], [int]):
-    return [], []
 
 
 # input batch should be in shape [batch_size, seq_length],
